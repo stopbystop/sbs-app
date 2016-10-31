7e291c3a5934cb8500cea60e501e55c2c07e2e0c
Alex Bulankou <alexb@stopbystop.com>
2016-10-30 23:02:00 -0700

    Refactoring into a separate client project

---
+++ src/client/gulpfile.js
@@ -0,0 +1,98 @@
+"use strict";
+
+var gulp = require("gulp"),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-cssmin"),
+    htmlmin = require("gulp-htmlmin"),
+    uglify = require("gulp-uglify"),
+    merge = require("merge-stream"),
+    del = require("del"),
+    bundleconfig = require("./bundleconfig.json"); // make sure bundleconfig.json doesn't contain any comments
+
+gulp.task("min", ["concat:js", "concat:css", "min:js", "min:css", "min:html"]);
+gulp.task("build-", ["min"]);
+gulp.task("build-Debug", ["min"]);
+gulp.task("build-Release", ["min"]);
+
+gulp.task("minWebBundle:js", function () {
+    return gulp.src(["OutScripts/webBundle.js"])
+        .pipe(concat("OutScripts/webBundle.min.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min:js", function () {
+    var tasks = getBundles(".js").map(function (bundle) {
+        return gulp.src(bundle.inputFiles, { base: "." })
+            .pipe(concat(bundle.outputFileName + ".min.js"))
+            .pipe(uglify())
+            .pipe(gulp.dest("."));
+    });
+    return merge(tasks);
+});
+
+gulp.task("concat:js", function () {
+    var tasks = getBundles(".js").map(function (bundle) {
+        return gulp.src(bundle.inputFiles, { base: "." })
+            .pipe(concat(bundle.outputFileName))
+            .pipe(gulp.dest("."));
+    });
+    return merge(tasks);
+});
+
+gulp.task("min:css", function () {
+    var tasks = getBundles(".css").map(function (bundle) {
+        return gulp.src(bundle.inputFiles, { base: "." })
+            .pipe(concat(bundle.outputFileName + ".min.css"))
+            .pipe(cssmin())
+            .pipe(gulp.dest("."));
+    });
+    return merge(tasks);
+});
+
+gulp.task("concat:css", function () {
+    var tasks = getBundles(".css").map(function (bundle) {
+        return gulp.src(bundle.inputFiles, { base: "." })
+            .pipe(concat(bundle.outputFileName))
+            .pipe(gulp.dest("."));
+    });
+    return merge(tasks);
+});
+
+gulp.task("min:html", function () {
+    var tasks = getBundles(".html").map(function (bundle) {
+        return gulp.src(bundle.inputFiles, { base: "." })
+            .pipe(concat(bundle.outputFileName + ".html"))
+            .pipe(htmlmin({ collapseWhitespace: true, minifyCSS: true, minifyJS: true }))
+            .pipe(gulp.dest("."));
+    });
+    return merge(tasks);
+});
+
+gulp.task("clean", function () {
+    var files = bundleconfig.map(function (bundle) {
+        return bundle.outputFileName;
+    });
+
+    return del(files);
+});
+
+gulp.task("watch", function () {
+    getBundles(".js").forEach(function (bundle) {
+        gulp.watch(bundle.inputFiles, ["min:js"]);
+    });
+
+    getBundles(".css").forEach(function (bundle) {
+        gulp.watch(bundle.inputFiles, ["min:css"]);
+    });
+
+    getBundles(".html").forEach(function (bundle) {
+        gulp.watch(bundle.inputFiles, ["min:html"]);
+    });
+});
+
+function getBundles(extension) {
+    return bundleconfig.filter(function (bundle) {
+        return new RegExp(extension).test(bundle.outputFileName);
+    });
+}
--- src/web/App_Start/BundleConfig.cs
+++ src/web/App_Start/BundleConfig.cs
@@ -54,38 +54,8 @@ namespace Yojowa.StopByStop.Web
 
 
             var scriptBundle = new ScriptBundle(string.Format(nonCdnFormat, "/client/content/sbsjsbundle"), string.Format(cdnFormat, "/client/content/sbsjsbundle")).Include(
-                        "~/Client/Scripts/jquery.easing.1.3.js",
-                        "~/Client/Scripts/jquery.jBreadCrumb.1.1.js",
-                        "~/Client/Scripts/rrssb.min.js",
-                        "~/Client/Scripts/jquery.verticalslider.js",
-                        "~/Client/Scripts/svg4everybody.js",
-                        "~/Client/Scripts/extensions.js",
-                        "~/Client/Scripts/stopbystop-kocustombindings.js",
-                        "~/Client/Scripts/stopbystop.js",
-                        "~/Client/Scripts/stopbystop-interfaces.js",
-                        "~/Client/Scripts/InitUrls.js",
-                        "~/Client/Scripts/Telemetry.js",
-                        "~/Client/Scripts/Utils.js",
-                        "~/Client/Scripts/Init.js",
-                        "~/Client/Scripts/ViewModels/LocationViewModel.js",
-                        "~/Client/Scripts/ViewModels/IStopPlace.js",
-                        "~/Client/Scripts/ViewModels/PoiCategoryViewModel.js",
-                        "~/Client/Scripts/ViewModels/FilterViewModel.js",
-                        "~/Client/Scripts/ViewModels/PoiImageViewModel.js",
-                        "~/Client/Scripts/ViewModels/ReviewGroupViewModel.js",
-                        "~/Client/Scripts/ViewModels/PoiViewModel.js",
-                        "~/Client/Scripts/ViewModels/PoiOnJunctionViewModel.js",
-                        "~/Client/Scripts/ViewModels/RouteStopViewModel.js",
-                        "~/Client/Scripts/ViewModels/RoutePlanViewModel.js",
-                        "~/Client/Scripts/ViewModels/JunctionViewModel.js",
-                        "~/Client/Scripts/ViewModels/RouteJunctionViewModel.js",
-                        "~/Client/Scripts/ViewModels/RouteSegmentViewModel.js",
-                        "~/Client/Scripts/ViewModels/RouteViewModel.js",
-                        "~/Client/Scripts/ViewModels/IAppViewModel.js",
-                        "~/Client/Scripts/ViewModels/AppViewModel.js",
-                        "~/Client/Scripts/ViewModels/JunctionMapViewModel.js",
-                        "~/Client/Scripts/ViewModels/JunctionAppViewModel.js",
-                        "~/Client/Scripts/ViewModels/SideBarViewModel.js"
+                        "~/client/scripts/sbsbundle.js",
+                        "~/client/scripts/webbundle.js"
                         );
 
 #if !DEBUG
@@ -96,9 +66,9 @@ namespace Yojowa.StopByStop.Web
 
 
             var stylesBundle = new StyleBundle(string.Format(nonCdnFormat, "/client/content/sbscssbundle"), string.Format(cdnFormat, "/client/content/sbscssbundle")).Include(
-                        "~/Client/content/styles.css",
-                        "~/Client/content/BreadCrumb.css",
-                        "~/Client/content/rrssb.css"
+                        "~/client/content/styles.css",
+                        "~/client/content/BreadCrumb.css",
+                        "~/client/content/rrssb.css"
                         );
 
 #if !DEBUG
@@ -109,7 +79,7 @@ namespace Yojowa.StopByStop.Web
 
 
             var stylesDelayedBundle = new StyleBundle(string.Format(nonCdnFormat, "/client/content/sbscssdelayedbundle"), string.Format(cdnFormat, "/client/content/sbscssdelayedbundle")).Include(
-                      "~/Client/content/styles_delayed.css"
+                      "~/client/content/styles_delayed.css"
                       );
 
             stylesDelayedBundle.Transforms.Add(new SBSCssMinify());
@@ -119,24 +89,6 @@ namespace Yojowa.StopByStop.Web
 #if !DEBUG
             BundleTable.EnableOptimizations = true;
 #endif
-
-            /*
-             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Client/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Client/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Client/Scripts/bootstrap.js",
-                      "~/Client/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Client/Content/css").Include(
-                      "~/Client/Content/bootstrap.css",
-                      "~/Client/Content/site.css"));
-            */
         }
     }
 }
