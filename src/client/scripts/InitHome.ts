47557ab67d5864c829a59ad06ad2e209520812f8
Alex Bulankou <bulankou@gmail.com>
2016-11-18 04:58:31 -0800

    Nearby places UI changes (#22)
    * Update index.html
    
    * Add an image div
    
    * Addition of classes that support image transition
    
    * Changes to inithome.ts
    
    Changes to inithome.ts to support dynamic div addition based on the nearby places returned.
    
    * div images moved to home.handlebars
    
    * Incremental changes and refactoring
    
    * css changes
    
    * removal of index.html changes done earlier
    
    * reverting index.html
    
    * Code cleanup and addressing Alex's review comments
    
    * Style changes
    
    * Updation to the correct file
    
    * Changes to address Alex's review comments
    
    * Reverting to original,exact index.css, as per Alex's comments
    
    * Minor formatting changes to styles.css
    
    * image holder class addition outside media screen directive

--- src/client/scripts/InitHome.ts
+++ src/client/scripts/InitHome.ts
@@ -1,4 +1,4 @@
-﻿/// <reference path="tsdef/jquery.d.ts"/>
+/// <reference path="tsdef/jquery.d.ts"/>
 /// <reference path="tsdef/jquerymobile.d.ts"/>
 /// <reference path="tsdef/knockout-3.3.d.ts"/>
 /// <reference path="AppState.ts" />
@@ -10,8 +10,116 @@
 module StopByStop {
 
     export class InitHome {
+        static yIncrement : number =-100; 
+        //The functionality below pulls the image url based on lat long
+        //And populates the images inside a div
+        //If an image is not available, it is shown as a blank div
+        public static addImagesDynamically(prevPlace,currentLocationString) {
+            
+            if (StopByStop.AppState.current.app !== StopByStop.SBSApp.Web) {
+                var prevPlace = $('#Images').data('prevPlace');                
+                var place = $('#from').data('place');
+                if (place === null) {
+                    return true;
+                }
+                if (prevPlace && prevPlace.n === place.n) {
+                    // Dont proceed further as the same place is being selected
+                    return true;
+                }
+                // Continue with the processing if the previous place doesnt match with the current returned place
+                $('#Images').data('prevPlace',place);
+                //Remove any div contents from the previous population before adding new divs
+                $("#Images").empty();
+                var placesNearbyUrl = "";
+                //If the place returned is the current location, the processing should be different, 
+                //should be picked up from place.i
+                if (place.n === currentLocationString) {
+                    var modifiedCurrentLocation = place.i.replace(",", "/");
+                    placesNearbyUrl = AppState.current.urls.PlacesNearbyUrl + modifiedCurrentLocation;
+                }
+                else {
+                    placesNearbyUrl = AppState.current.urls.PlacesNearbyUrl + place.l.a + '/' + place.l.o;
+                }
+                $.ajax({
+                    url: placesNearbyUrl,
+                    dataType: 'json',
+                    method: 'GET',
+                    success: function (result) {
+                        var imageDiv = $("#Images"), myDivs = [], divIndex = 0, numOfDivs = result.length;
+                        if (numOfDivs > 10 && window.screen.width > 480) {
+                            numOfDivs = 10; //Restrict results to 10 for a desktop screen
+                        }
+                        if (numOfDivs > 6 && window.screen.width < 480) {
+                            numOfDivs = 6; //Restrict results to 6 for a mobile screen
+                        }
+                        for (divIndex; divIndex < numOfDivs; divIndex += 1) {
+                            var divId = "appendedImagediv" + divIndex;
+                            var imageId = "appendedImageId" + divIndex;
+                            myDivs.push(InitHome.createDiv(divIndex, result[divIndex]));
+                            $(myDivs[divIndex]).data('place', result[divIndex]);
+                            $("#Images").append(myDivs[divIndex]);
+                            $(myDivs[divIndex]).on('click', function () {
+
+                                $("#to").val($(this).data('place').n);
+                                var placeData = { n: $(this).data('place').n, i: $(this).data('place').i };
+                                $("#to").data('place', placeData);
+                                $("#view_trip").removeClass("ui-disabled");
+
+                            });
+                            
+                            var imgurl=StopByStop.AppState.current.urls.CityImagesUrl + result[divIndex].i + '.jpg';
+                            $('#appendedImagediv' + divIndex).css('background-image','url(' + imgurl + ')');                            
+                            
+                        }
+                        
+                        InitHome.moveImageInBackground();
+                    }
+                });
+            }
+        }
+        
+        public static moveImageInBackground(){
+            InitHome.yIncrement=InitHome.yIncrement+1;
+            var divIndexCount=0;
+            var requestID = null;
+            for(divIndexCount=0;divIndexCount<10;divIndexCount++){
+                var appendedImagediv = $('#appendedImagediv'+divIndexCount);
+                if(appendedImagediv){
+                    $(appendedImagediv).css('background-position','0px ' + InitHome.yIncrement + 'px');
+                }
+            }
+            requestID=requestAnimationFrame(InitHome.moveImageInBackground);
+            if(InitHome.yIncrement>=0)
+            {
+                cancelAnimationFrame(requestID); 
+                InitHome.yIncrement=-100;
+            }
+            
+        }        
+
+        public static createDiv(divIndex,data) {
+            var imageDiv = document.createElement("div");
+            imageDiv.id = 'appendedImagediv' + divIndex;
+            imageDiv.className = "imageholder";
+            var innerDiv = document.createElement("div");
+            innerDiv.id = 'appendedInnerdiv' + divIndex;
+            innerDiv.className = "childdiv";
+            innerDiv.innerHTML = data.n;
+            imageDiv.appendChild(innerDiv);
+            innerDiv.onclick = function () {
+                $("#to").val($(this).parent('div').data('place').n);
+                var placeData = { n: $(this).parent('div').data('place').n, i: $(this).parent('div').data('place').i };
+                $("#to").data('place', placeData);
+                $("#view_trip").removeClass("ui-disabled");
+            };
+            return imageDiv;
+        }
+        
         public static wireup() {
             var currentLocationString = "Current location";
+            var fromCityListData = null;
+            var prevPlace = null;
+            
             // populate from with 'current location'
             var currentLocationData = null;
             var setCurrentLocation = function () {
@@ -20,12 +128,16 @@ module StopByStop {
                         (position: Position) => {
                             var srcLat = position.coords.latitude;
                             var srcLon = position.coords.longitude;
-
                             if (srcLat > S_LAT_BOUNDARY && srcLat < N_LAT_BOUNDARY && srcLon > W_LON_BOUNDARY && srcLon < E_LON_BOUNDARY) {
                                 currentLocationData = { n: currentLocationString, i: srcLat.toFixed(5) + "," + srcLon.toFixed(5) };
 
                                 $("#from").val(currentLocationString);
                                 $("#from").data({ place: currentLocationData });
+                                
+                                if($("#from").data('place'))
+                                {
+                                    InitHome.addImagesDynamically(prevPlace,currentLocationString);
+                                }
 
                                 Telemetry.trackEvent(TelemetryEvent.LocationIN);
                             }
@@ -77,8 +189,8 @@ module StopByStop {
                         dataType: 'json',
                         method: 'GET',
                         success: function (data) {
+                            fromCityListData = data;
                             $wrapper.find('.ui-loader').remove();
-                           
                             $ul.empty();
                             var otherDropDown = $(that).attr('id') === 'from' ? $('#to') : $('#from');
                             var otherDropDownSelectedID = null;
@@ -110,6 +222,7 @@ module StopByStop {
 
                 var $input = $(this).closest('div').find('input');
                 var place = $(this).data('place');
+                
                 if (place) {
                     $input.val(place.n).data('place', place);
                     $(this).closest('ul').empty();
@@ -117,6 +230,8 @@ module StopByStop {
                     if ($("#from").data('place') && $("#to").data('place')) {
                         $("#view_trip").removeClass("ui-disabled");
                     }
+                    
+                    InitHome.addImagesDynamically(prevPlace,currentLocationString);
                 }
 
                 Telemetry.trackEvent(TelemetryEvent.CityDropdownClick);
@@ -151,6 +266,7 @@ module StopByStop {
                         /* navigate without using AJAX navigation */
                         window.location.assign(url);
                     } else {
+
                         Utils.spaPageNavigate(SBSPage.route, startlocation.i + '-to-' + endlocation.i);
                     }
                 }
@@ -159,6 +275,7 @@ module StopByStop {
             if ($("#from").data('place') && $("#to").data('place')) {
                 $("#view_trip").removeClass("ui-disabled");
             }
-        }
+            
+         }
     }
 }
