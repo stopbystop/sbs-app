531f40a11f5988bf60a951ede09756676705bd63
Alex Bulankou <alexb@stopbystop.com>
2017-02-10 06:49:36 -0800

    interfaces update

---
+++ src/interfaces/ObjectProperty.cs
@@ -0,0 +1,11 @@
+﻿namespace Yojowa.StopByStop
+{
+    public class ObjectProperty<T>
+    {
+        public ObjectProperty(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; private set; }
+    }
+}
--- src/interfaces/Poi2.cs
+++ src/interfaces/Poi2.cs
@@ -1,16 +1,9 @@
-﻿using Newtonsoft.Json;
-
-namespace Yojowa.StopByStop
+﻿namespace Yojowa.StopByStop
 {
-    public class Poi2
+    using Newtonsoft.Json;
+
+    public class Poi2 : ObjectWithProperties
     {
-
-        public Poi2()
-        {
-
-        }
-
-
         [JsonProperty("id")]
         public long ID { get; set; }
 
@@ -22,5 +15,20 @@ namespace Yojowa.StopByStop
 
         [JsonProperty("cc")]
         public CountryCode CountryCode { get; set; }
+
+        [JsonProperty("n")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the price category expressed in decimals from 0.5 to 5.0
+        /// </summary>
+        /// <value>
+        /// The price category.
+        /// </value>
+        [JsonProperty("pc")]
+        public double PriceCategory { get; set; }
+
+        [JsonProperty("nh")]
+        public string Neighborhood { get; set; }
     }
 }
