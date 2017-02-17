4263fcf94377e5d52d94b1a97da153eef2ccc400
Alex Bulankou <albulank@microsoft.com>
2017-02-17 09:47:27 -0800

    Conditional service build

---
+++ src/interfaces/RootPoiCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace Yojowa.StopByStop
+{
+    public enum RootPoiCategory
+    {
+        Active = 5,
+        Auto = 60,
+        Food = 371,
+        Hotels = 495,
+        Restaurants = 835
+    }
+}
