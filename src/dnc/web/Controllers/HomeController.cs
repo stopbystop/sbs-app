0e3886b7dd23463754c97719b01543e1a48f603a
Alex Bulankou <alexb@stopbystop.com>
2017-08-06 08:45:55 -0700

    Continuing .NET Core conversion

---
+++ src/dnc/web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿namespace Yojowa.StopByStop.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+    using Service;
+
+    [NoCache]
+    public class HomeController : Controller
+    {
+        public ActionResult Index (string rf)
+        {
+            return View ("~/client/Views/Main.cshtml", new MainModel (StopByStopService.RouteServiceInstance.GetMetadata (), this.Url)
+            {
+                Page = ClientPage.Home
+            });
+        }
+
+        public ActionResult About ()
+        {
+            return View ("~/client/Views/Main.cshtml", new MainModel (StopByStopService.RouteServiceInstance.GetMetadata (), this.Url)
+            {
+                Page = ClientPage.About
+            });
+        }
+    }
+}
